46dec88984615621344053eca5a211a881e6f665
Jeongbong Seo <jngbng@gmail.com>
2020-08-27 23:40:49 +0900

    안쓰는 staticQuery 제거

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,21 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
 
 import Header from "./header";
 import Footer from "./footer";
 
-const Layout = ({ isFloatMenu, closingEmoji, closingMsg, children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  return (
-    <>
-      <Header
-        isFloatMenu={isFloatMenu}
-      />
-      <main>{children}</main>
-      <Footer
-        closingEmoji={closingEmoji}
-        closingMsg={closingMsg}
-      />
-    </>
-  );
-};
+const Layout = ({ isFloatMenu, closingEmoji, closingMsg, children }) => (
+  <>
+    <Header
+      isFloatMenu={isFloatMenu}
+    />
+    <main>{children}</main>
+    <Footer
+      closingEmoji={closingEmoji}
+      closingMsg={closingMsg}
+    />
+  </>
+);
 
 Layout.propTypes = {
   isFloatMenu: PropTypes.bool,
