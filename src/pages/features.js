7e081139f48f0b32becbebadd7b0d74792092e2f
Jeongbong Seo <jngbng@gmail.com>
2020-09-16 15:40:54 +0900

    페이지에서 링크 새로 고침 시 link id 망가지는 문제 수정

--- src/pages/features.js
+++ src/pages/features.js
@@ -15,6 +15,23 @@ import iconMenuSummary from "../images/features-menu-summary.svg";
 import iconMenuStatus from "../images/features-menu-status.svg";
 import iconMenuLocationmode from "../images/features-menu-locationmode.svg";
 
+
+// function.name 으로 했더니 해당 화면에서 새로고침 했을 때 망가짐.
+// navigation으로 들어가면 잘 동작하나 새로고침하면 id 부분은 mangle된 이름을 쓰고, link 부분에서는 원래 이름을 씀.
+const idFeatureExpiry = "feature-expiry";
+const idFeatureLowstock = "feature-lowstock";
+const idFeatureBarcodelabel = "feature-barcodelabel";
+const idFeatureSummary = "feature-summary";
+const idFeatureStatus = "feature-status";
+const idFeatureLocation = "feature-location";
+
+const bgOrange = "rgba(245, 166, 35, 0.2)";
+const barOrange = "#f5a623";
+const bgGreen = "rgba(74, 208, 81, 0.2)";
+const barGreen = "#3cb9a0";
+const bgBlue= "#f7f9fc";
+const barBlue = "#50a4fa";
+
 const MenuItem = ({to, icon, label}) => (
   <Link
     to={to}
@@ -35,32 +52,32 @@ const MenuItem = ({to, icon, label}) => (
 const Menu = () => (
   <Container1024 className={styles.menuContainer}>
     <MenuItem
-      to={`#${FeatureExpiry.name}`}
+      to={`#${idFeatureExpiry}`}
       icon={iconMenuExpiry}
       label="유통기한 관리"
     />
     <MenuItem
-      to={`#${FeatureLowstock.name}`}
+      to={`#${idFeatureLowstock}`}
       icon={iconMenuEmpty}
       label="안전재고"
     />
     <MenuItem
-      to={`#${FeatureBarcodelabel.name}`}
+      to={`#${idFeatureBarcodelabel}`}
       icon={iconMenuCustomization}
       label={<>바코드<br/>커스터마이징</>}
     />
     <MenuItem
-      to={`#${FeatureSummary.name}`}
+      to={`#${idFeatureSummary}`}
       icon={iconMenuSummary}
       label="입출고 요약"
     />
     <MenuItem
-      to={`#${FeatureStatus.name}`}
+      to={`#${idFeatureStatus}`}
       icon={iconMenuStatus}
       label="상태 관리"
     />
     <MenuItem
-      to={`#${FeatureLocation.name}`}
+      to={`#${idFeatureLocation}`}
       icon={iconMenuLocationmode}
       label="위치 관리"
     />
@@ -106,7 +123,7 @@ function FeatureTemplate(props) {
               className={styles.featureTemplateTitle}
               data-sal="slide-up-10"
               data-sal-duration="500"
-              data-sal-easing="ease"
+              data-sal-easing="easeOutQuint"
             >
               {props.title}
             </div>
@@ -116,7 +133,7 @@ function FeatureTemplate(props) {
               data-sal="slide-up-10"
               data-sal-duration="500"
               data-sal-delay="300"
-              data-sal-easing="ease"
+              data-sal-easing="easeOutQuint"
             >
               {props.desc}
             </div>
@@ -125,7 +142,7 @@ function FeatureTemplate(props) {
             className={styles.featureTemplateFigureContainer}
             data-sal="slide-up-10"
             data-sal-duration="500"
-            data-sal-easing="ease"
+            data-sal-easing="easeOutQuint"
           >
             <Img
               fixed={props.figure}
@@ -140,7 +157,7 @@ function FeatureTemplate(props) {
           data-sal="slide-up-10"
           data-sal-duration="500"
           data-sal-delay="300"
-          data-sal-easing="ease">
+          data-sal-easing="easeOutQuint">
           {props.demoData.slice(0, 2).map((data, index) => (
             <DemoTemplate
               key={index}
@@ -156,174 +173,155 @@ function FeatureTemplate(props) {
   );
 }
 
-const bgOrange = "rgba(245, 166, 35, 0.2)";
-const barOrange = "#f5a623";
-const bgGreen = "rgba(74, 208, 81, 0.2)";
-const barGreen = "#3cb9a0";
-const bgBlue= "#f7f9fc";
-const barBlue = "#50a4fa";
+const FeatureExpiry = (props) => (
+  <FeatureTemplate
+    id={idFeatureExpiry}
+    bgColor={bgOrange}
+    title="유통기한 관리"
+    figure={props.data.expiryFig.childImageSharp.fixed}
+    figureStyle={{
+    }}
+    desc={<>제품에 유통기한을 설정해 관리할 수 있습니다.<br/>같은 제품도 유통기한을 다르게 설정해 한 번에 재고를 파악할<br />수 있으며 유통기한이 임박한 재고에 대해 푸시 알림을 받을 수<br/> 있습니다.</>}
+    barColor={barOrange}
+    demoData={[
+      {
+        icon: props.data.expiryDemo1.childImageSharp.fixed,
+        title: "럭키식품마트",
+        desc: (<>“같은 식품을 유통기한 별로 나눠 관리할 수 있고 유통기한이<br/>임박한 식품은 푸시 알림을 받아 할인행사를 진행해 폐기를<br/>줄이고 있어요.”</>),
+      },
+      {
+        icon: props.data.expiryDemo2.childImageSharp.fixed,
+        title: "별다방카페",
+        desc: (<>“음료를 제조할 때 필요한 원두, 유제품, 과일 등의 제품에<br/>유통기한 정보를 등록해 재고를 효율적으로 관리하고 있습니다.”</>),
+      },
+    ]}
+  />
+);
 
-function FeatureExpiry(props) {
-  return (
-    <FeatureTemplate
-      id={FeatureExpiry.name}
-      bgColor={bgOrange}
-      title="유통기한 관리"
-      figure={props.data.expiryFig.childImageSharp.fixed}
-      figureStyle={{
-      }}
-      desc={<>제품에 유통기한을 설정해 관리할 수 있습니다.<br/>같은 제품도 유통기한을 다르게 설정해 한 번에 재고를 파악할<br />수 있으며 유통기한이 임박한 재고에 대해 푸시 알림을 받을 수<br/> 있습니다.</>}
-      barColor={barOrange}
-      demoData={[
-        {
-          icon: props.data.expiryDemo1.childImageSharp.fixed,
-          title: "럭키식품마트",
-          desc: (<>“같은 식품을 유통기한 별로 나눠 관리할 수 있고 유통기한이<br/>임박한 식품은 푸시 알림을 받아 할인행사를 진행해 폐기를<br/>줄이고 있어요.”</>),
-        },
-        {
-          icon: props.data.expiryDemo2.childImageSharp.fixed,
-          title: "별다방카페",
-          desc: (<>“음료를 제조할 때 필요한 원두, 유제품, 과일 등의 제품에<br/>유통기한 정보를 등록해 재고를 효율적으로 관리하고 있습니다.”</>),
-        },
-      ]}
-    />
-  );
-}
+const FeatureLowstock = (props) => (
+  <FeatureTemplate
+    id={idFeatureLowstock}
+    bgColor={bgGreen}
+    title="안전재고"
+    figure={props.data.lowstockFig.childImageSharp.fixed}
+    figureStyle={{
+    }}
+    desc={<>재고로 보유하고 있어야 할 최소 수량을 제품 별로 입력할 수<br/>있습니다. 제품 별로 부족한 수량을 쉽게 확인할 수 있으며<br/>매일 아침 9시에 부족한 제품에 대해 푸시 알림을 받을 수 있습<br/>니다.</>}
+    barColor={barGreen}
+    demoData={[
+      {
+        icon: props.data.lowstockDemo1.childImageSharp.fixed,
+        title: "튼튼타일",
+        desc: (<>“자재 별로 꼭 있어야 할 최소 수량을 설정하고 알림을 통해<br/>재고량을 항상 일정 수준 이상으로 유지할 수 있어 자재가<br/>부족해 발생하는 문제를 최소화했습니다.”</>),
+      },
+      {
+        icon: props.data.lowstockDemo2.childImageSharp.fixed,
+        title: "대박식당",
+        desc: (<>“음식점을 운영하는 데 꼭 필요한 식용유, 휴지와 같은 물품이<br/>전부 소진되기 전에 알림을 받을 수 있어 필요한 물품을 놓치지<br/>않고 주문하고 있습니다.”</>),
+      },
+    ]}
+  />
+);
 
-function FeatureLowstock(props) {
-  return (
-    <FeatureTemplate
-      id={FeatureLowstock.name}
-      bgColor={bgGreen}
-      title="안전재고"
-      figure={props.data.lowstockFig.childImageSharp.fixed}
-      figureStyle={{
-      }}
-      desc={<>재고로 보유하고 있어야 할 최소 수량을 제품 별로 입력할 수<br/>있습니다. 제품 별로 부족한 수량을 쉽게 확인할 수 있으며<br/>매일 아침 9시에 부족한 제품에 대해 푸시 알림을 받을 수 있습<br/>니다.</>}
-      barColor={barGreen}
-      demoData={[
-        {
-          icon: props.data.lowstockDemo1.childImageSharp.fixed,
-          title: "튼튼타일",
-          desc: (<>“자재 별로 꼭 있어야 할 최소 수량을 설정하고 알림을 통해<br/>재고량을 항상 일정 수준 이상으로 유지할 수 있어 자재가<br/>부족해 발생하는 문제를 최소화했습니다.”</>),
-        },
-        {
-          icon: props.data.lowstockDemo2.childImageSharp.fixed,
-          title: "대박식당",
-          desc: (<>“음식점을 운영하는 데 꼭 필요한 식용유, 휴지와 같은 물품이<br/>전부 소진되기 전에 알림을 받을 수 있어 필요한 물품을 놓치지<br/>않고 주문하고 있습니다.”</>),
-        },
-      ]}
-    />
-  );
-}
+const FeatureBarcodelabel = (props) => (
+  <FeatureTemplate
+    id={idFeatureBarcodelabel}
+    bgColor={bgBlue}
+    title="바코드 커스터마이징"
+    figure={props.data.barcodelabelFig.childImageSharp.fixed}
+    figureStyle={{
+    }}
+    desc={<>나에게 맞는 바코드를 쉽게 만들 수 있습니다.<br/>다양한 바코드 템플릿을 제공하며 원하는 템플릿이 없을 경우<br/>세부 설정을 통해 원하는 정보를 모두 담은 바코드를 만들어<br/>프린터 연동을 통해 인쇄할 수 있습니다.</>}
+    barColor={barBlue}
+    demoData={[
+      {
+        icon: props.data.barcodelabelDemo1.childImageSharp.fixed,
+        title: "핸드메이드제이",
+        desc: (<>“바코드가 없는 수제로 만든 악세서리에 바코드를 생성하고<br/>바코드 커스터마이징을 통해 제품 재질 정보를 바코드와 함께<br/>인쇄한 뒤 제품에 부착해 재고를 효율적으로 관리하고 있어요.”</>),
+      },
+      {
+        icon: props.data.barcodelabelDemo2.childImageSharp.fixed,
+        title: "인조이프라모델",
+        desc: (<>“기존 제품의 바코드를 활용해 스캔 한 번으로 제품을 등록할<br/>수 있고 재고를 검색할 때도 바코드 스캔을 활용해 해당 제품의<br/>재고 내역을 빠르게 찾아볼 수 있습니다.”</>),
+      },
+    ]}
+  />
+);
 
-function FeatureBarcodelabel(props) {
-  return (
-    <FeatureTemplate
-      id={FeatureBarcodelabel.name}
-      bgColor={bgBlue}
-      title="바코드 커스터마이징"
-      figure={props.data.barcodelabelFig.childImageSharp.fixed}
-      figureStyle={{
-      }}
-      desc={<>나에게 맞는 바코드를 쉽게 만들 수 있습니다.<br/>다양한 바코드 템플릿을 제공하며 원하는 템플릿이 없을 경우<br/>세부 설정을 통해 원하는 정보를 모두 담은 바코드를 만들어<br/>프린터 연동을 통해 인쇄할 수 있습니다.</>}
-      barColor={barBlue}
-      demoData={[
-        {
-          icon: props.data.barcodelabelDemo1.childImageSharp.fixed,
-          title: "핸드메이드제이",
-          desc: (<>“바코드가 없는 수제로 만든 악세서리에 바코드를 생성하고<br/>바코드 커스터마이징을 통해 제품 재질 정보를 바코드와 함께<br/>인쇄한 뒤 제품에 부착해 재고를 효율적으로 관리하고 있어요.”</>),
-        },
-        {
-          icon: props.data.barcodelabelDemo2.childImageSharp.fixed,
-          title: "인조이프라모델",
-          desc: (<>“기존 제품의 바코드를 활용해 스캔 한 번으로 제품을 등록할<br/>수 있고 재고를 검색할 때도 바코드 스캔을 활용해 해당 제품의<br/>재고 내역을 빠르게 찾아볼 수 있습니다.”</>),
-        },
-      ]}
-    />
-  );
-}
+const FeatureSummary = (props) => (
+  <FeatureTemplate
+    id={idFeatureSummary}
+    bgColor={bgOrange}
+    title="입출고 요약"
+    figure={props.data.summaryFig.childImageSharp.fixed}
+    figureStyle={{
+    }}
+    desc={<>특정 기간의 입출고 내역을 한 눈에 확인할 수 있습니다.<br/>설정한 기간 내에서 제품 이름 뿐만 아니라<br/>다양한 속성을 검색해 원하는 정보만 쉽게 필터링할 수 있고<br/>요약 결과를 엑셀로 다운로드할 수 있습니다.</>}
+    barColor={barOrange}
+    demoData={[
+      {
+        icon: props.data.summaryDemo1.childImageSharp.fixed,
+        title: "지피타이어",
+        desc: (<>“수많은 내역 중에서 내가 원하는 기간과 제품 유형을 선택해<br/>입출고 내역을 요약해서 볼 수 있어 필요한 정보를 얻는 데<br/>걸리는 시간을 크게 줄였습니다.”</>),
+      },
+      {
+        icon: props.data.summaryDemo2.childImageSharp.fixed,
+        title: "더뷰티샵",
+        desc: (<>“기간 별로 어떤 제품이 얼마나 입고되고 출고되었는지 한 눈에<br/>파악할 수 있고 필요한 경우 엑셀파일로 추출할 수 있어 편리하게<br/>입출고 내역을 확인하고 있어요.”</>),
+      },
+    ]}
+  />
+);
 
-function FeatureSummary(props) {
-  return (
-    <FeatureTemplate
-      id={FeatureSummary.name}
-      bgColor={bgOrange}
-      title="입출고 요약"
-      figure={props.data.summaryFig.childImageSharp.fixed}
-      figureStyle={{
-      }}
-      desc={<>특정 기간의 입출고 내역을 한 눈에 확인할 수 있습니다.<br/>설정한 기간 내에서 제품 이름 뿐만 아니라<br/>다양한 속성을 검색해 원하는 정보만 쉽게 필터링할 수 있고<br/>요약 결과를 엑셀로 다운로드할 수 있습니다.</>}
-      barColor={barOrange}
-      demoData={[
-        {
-          icon: props.data.summaryDemo1.childImageSharp.fixed,
-          title: "지피타이어",
-          desc: (<>“수많은 내역 중에서 내가 원하는 기간과 제품 유형을 선택해<br/>입출고 내역을 요약해서 볼 수 있어 필요한 정보를 얻는 데<br/>걸리는 시간을 크게 줄였습니다.”</>),
-        },
-        {
-          icon: props.data.summaryDemo2.childImageSharp.fixed,
-          title: "더뷰티샵",
-          desc: (<>“기간 별로 어떤 제품이 얼마나 입고되고 출고되었는지 한 눈에<br/>파악할 수 있고 필요한 경우 엑셀파일로 추출할 수 있어 편리하게<br/>입출고 내역을 확인하고 있어요.”</>),
-        },
-      ]}
-    />
-  );
-}
+const FeatureStatus = (props) => (
+  <FeatureTemplate
+    id={idFeatureStatus}
+    bgColor={bgGreen}
+    title="상태 관리"
+    figure={props.data.statusFig.childImageSharp.fixed}
+    figureStyle={{
+    }}
+    desc={<>유닛 모드를 사용해 동일한 제품에 유통기한, 품질 등 각자 다른<br/>상태 값을 부여할 수 있습니다.<br/>모든 정보를 입력할 필요없이 상태 값만 입력하면 해당 제품이<br/>추가되고 같은 상태 값끼리 묶어 재고를 관리할 수 있습니다.</>}
+    barColor={barGreen}
+    demoData={[
+      {
+        icon: props.data.statusDemo1.childImageSharp.fixed,
+        title: "언니네옷가게",
+        desc: (<>“유닛모드를 사용해 같은 제품이지만 불량인 제품을 따로 저장<br/>하고 상태값 정보를 모두 담고 있는 QR코드를 붙여 효율적으로<br/>관리하고 있어요.”</>),
+      },
+      {
+        icon: props.data.statusDemo2.childImageSharp.fixed,
+        title: "건강한약국",
+        desc: (<>“같은 의약품이더라도 다른 값을 가지고 있는 유통기한,<br/>보관위치, 제조번호 등을 각각의 의약품마다 따로 저장해<br/>편리하게 의약품 재고를 관리하고 있습니다.”</>),
+      },
+    ]}
+  />
+);
 
-function FeatureStatus(props) {
-  return (
-    <FeatureTemplate
-      id={FeatureStatus.name}
-      bgColor={bgGreen}
-      title="상태 관리"
-      figure={props.data.statusFig.childImageSharp.fixed}
-      figureStyle={{
-      }}
-      desc={<>유닛 모드를 사용해 동일한 제품에 유통기한, 품질 등 각자 다른<br/>상태 값을 부여할 수 있습니다.<br/>모든 정보를 입력할 필요없이 상태 값만 입력하면 해당 제품이<br/>추가되고 같은 상태 값끼리 묶어 재고를 관리할 수 있습니다.</>}
-      barColor={barGreen}
-      demoData={[
-        {
-          icon: props.data.statusDemo1.childImageSharp.fixed,
-          title: "언니네옷가게",
-          desc: (<>“유닛모드를 사용해 같은 제품이지만 불량인 제품을 따로 저장<br/>하고 상태값 정보를 모두 담고 있는 QR코드를 붙여 효율적으로<br/>관리하고 있어요.”</>),
-        },
-        {
-          icon: props.data.statusDemo2.childImageSharp.fixed,
-          title: "건강한약국",
-          desc: (<>“같은 의약품이더라도 다른 값을 가지고 있는 유통기한,<br/>보관위치, 제조번호 등을 각각의 의약품마다 따로 저장해<br/>편리하게 의약품 재고를 관리하고 있습니다.”</>),
-        },
-      ]}
-    />
-  );
-}
-
-function FeatureLocation(props) {
-  return (
-    <FeatureTemplate
-      id={FeatureLocation.name}
-      bgColor={bgBlue}
-      title="위치 관리"
-      figure={props.data.locationFig.childImageSharp.fixed}
-      figureStyle={{
-      }}
-      desc={<>위치모드를 사용해 동일한 제품을 위치에 따라 관리할 수 있습<br/>니다. 위치를 기반으로 제품이 등록되고 입출고가 이루어져<br/>여러 창고를 효율적으로 관리할 수 있습니다.</>}
-      barColor={barBlue}
-      demoData={[
-        {
-          icon: props.data.locationDemo1.childImageSharp.fixed,
-          title: "패밀리가전가구",
-          desc: (<>“같은 제품을 여러 창고에서 관리하고 있는데 위치 모드를<br/>활용해 제품을 위치 별로 기록할 수 있고 이동하기 기능을<br/>통해 재고 위치를 쉽게 변경할 수 있어 재고를 놓치는 문제를<br/>최소화했습니다.”</>),
-        },
-        {
-          icon: props.data.locationDemo2.childImageSharp.fixed,
-          title: "비지피웍스",
-          desc: (<>“층, 사무실 등 위치 별로 비품을 관리할 수 있고 새로운 곳에서<br/>같은 비품을 사용할 때 위치만 추가하면 제품을 입고처리할 수<br/>있어 효율적인 재고관리가 가능합니다.”</>),
-        },
-      ]}
-    />
-  );
-}
+const FeatureLocation = (props) => (
+  <FeatureTemplate
+    id={idFeatureLocation}
+    bgColor={bgBlue}
+    title="위치 관리"
+    figure={props.data.locationFig.childImageSharp.fixed}
+    figureStyle={{
+    }}
+    desc={<>위치모드를 사용해 동일한 제품을 위치에 따라 관리할 수 있습<br/>니다. 위치를 기반으로 제품이 등록되고 입출고가 이루어져<br/>여러 창고를 효율적으로 관리할 수 있습니다.</>}
+    barColor={barBlue}
+    demoData={[
+      {
+        icon: props.data.locationDemo1.childImageSharp.fixed,
+        title: "패밀리가전가구",
+        desc: (<>“같은 제품을 여러 창고에서 관리하고 있는데 위치 모드를<br/>활용해 제품을 위치 별로 기록할 수 있고 이동하기 기능을<br/>통해 재고 위치를 쉽게 변경할 수 있어 재고를 놓치는 문제를<br/>최소화했습니다.”</>),
+      },
+      {
+        icon: props.data.locationDemo2.childImageSharp.fixed,
+        title: "비지피웍스",
+        desc: (<>“층, 사무실 등 위치 별로 비품을 관리할 수 있고 새로운 곳에서<br/>같은 비품을 사용할 때 위치만 추가하면 제품을 입고처리할 수<br/>있어 효율적인 재고관리가 가능합니다.”</>),
+      },
+    ]}
+  />
+);
 
 export const FeaturesPage = ({data}) => (
   <Layout
