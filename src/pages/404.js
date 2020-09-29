462d953a8442e89c7ed005d7e0b0c10a41d40edc
Jeongbong Seo <jngbng@gmail.com>
2020-09-29 12:33:34 +0900

    404 페이지 모바일 최적화

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,48 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { useI18next } from 'gatsby-plugin-react-i18next';
+import { useHelpscout } from '../components/helpscout';
 
 import DesktopLayout from "../components/desktop-layout";
+import MobileLayout from "../components/mobile-layout";
+import { Media } from "../media"
 import SEO from "../components/seo";
 
-const NotFoundPage = ({data}) => (
-  <DesktopLayout
-    isFloatMenu={false}
-    closingEmoji={data.dinosaur}
-    closingMsg="박스히어로는 오늘도 진화 중."
-  >
-    <SEO title="404: Not found" />
-    <h1 style={{margin: 50, textAlign: "center"}}>NOT FOUND</h1>
-    <p style={{margin: 50, textAlign: "center"}}>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </DesktopLayout>
-);
+const NotFoundPage = ({data}) => {
+  const { language, t } = useI18next();
+  useHelpscout();
+  return (
+    <>
+      <SEO
+        lang={language}
+        title="NOT FOUND"
+        description="NOT FOUND"
+      />
+
+      <Media at="xs">
+        <MobileLayout
+          isFloatMenu={false}
+          closingEmoji={data.mobileDinosaur}
+          closingMsg={t("features:closingMsg")}
+        >
+          <h1 style={{margin: 50, textAlign: "center"}}>NOT FOUND</h1>
+          <p style={{margin: 50, textAlign: "center"}}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </MobileLayout>
+      </Media>
+
+      <Media greaterThan="xs">
+        <DesktopLayout
+          isFloatMenu={false}
+          closingEmoji={data.dinosaur}
+          closingMsg={t("features:closingMsg")}
+        >
+          <h1 style={{margin: 50, textAlign: "center"}}>NOT FOUND</h1>
+          <p style={{margin: 50, textAlign: "center"}}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </DesktopLayout>
+      </Media>
+    </>
+  );
+};
 
 export default NotFoundPage;
 
@@ -23,6 +51,13 @@ export const query = graphql`
     dinosaur: file(relativePath: { eq: "emoji-dinosaur.png" }) {
       childImageSharp {
         fixed(width: 72, height: 72, fit: FILL) {
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    mobileDinosaur: file(relativePath: { eq: "emoji-dinosaur.png" }) {
+      childImageSharp {
+        fixed(width: 36, height: 36, fit: FILL) {
           ...GatsbyImageSharpFixed_withWebp
         }
       }
