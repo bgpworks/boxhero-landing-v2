ca0ab73aa35fc4043bccac4ec07c3e6307acfd08
eunjiSoh <ejsoh@bgpworks.com>
2020-10-18 20:33:39 +0900

    rootmarge 삭제

--- src/transition/index.js
+++ src/transition/index.js
@@ -10,7 +10,7 @@ function CommonTransition({
   // 최상단으로 이동하므로 한번만 전달해도 된다.
   is_mobile = null,
   force_load = false,
-  options = { once: true, rootMargin: "0% 50%" },
+  options = { once: true },
 }) {
   const [state, setState] = React.useState({ mobile: true, forceLoad: "" })
   // 이미 활성화 된 sal의 options를 초기화 하기 위해 reset method를 사용하여 viewport 사이즈에 따라 옵션을 제공한다.
@@ -22,12 +22,16 @@ function CommonTransition({
   // is_desktop의 인자가 바뀌면 인자를 상태에 저장하여 컴포넌트를 리렌더링 함,
   // sal의 옵션을 리셋하면 다시 sal이 관찰을 시작한다.
   React.useEffect(() => {
+    // 가끔 첫 컨텐츠의  reveal 시작점이 컨텐츠의 중간인 경우가 있다.
+    // 페이지에 진입하자마자 애니메이션이 시작되어야 하는데 시작되지 않아 컨텐츠가 보이지 않는다.
+    // 그런 경우, force_load옵션을 true로 줘서, 강제로 reveal시킨다.
     if (force_load) {
       setTimeout(() => {
         setState({ ...state, forceLoad: "sal-animate" })
       }, 600)
     }
     setState({ forceLoad: "", mobile: is_mobile })
+
   }, [is_mobile, force_load])
 
   return (
@@ -48,7 +52,6 @@ function TransitionUp({
   item, // 필수
   ...arg
 }) {
-  //   console.log(item)
   return CommonTransition({ direction: "slide-up", item: item, ...arg })
 }
 
