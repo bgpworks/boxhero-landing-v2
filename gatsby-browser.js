d29628712456735db0732cc7b93c58c188070ef0
ejsoh <ejsoh@bgpworks.com>
2020-10-20 13:07:08 +0900

    ie 브라우저 체크 하여 라이브러리 사용 여부 결정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -14,6 +14,21 @@ export const onClientEntry = () => {
     require("intersection-observer");
     console.log(`# IntersectionObserver is polyfilled!`);
   }
+  // CustomEvent polyfill for gatsby-plugin-scroll-reveal (IE)
+  if(! (typeof window.CustomEvent === "function")) {
+    (function () {
+      function CustomEvent ( event, params ) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt = document.createEvent( 'CustomEvent' );
+        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+        return evt;
+       };
+
+      CustomEvent.prototype = window.Event.prototype;
+
+      window.CustomEvent = CustomEvent;
+    })();
+  }
 
   // query param을 local storage에 쓴다.
   // 광고 트래킹을 위해 query param을 보관하고 외부로 나갈때 붙여서 나간다.
@@ -22,7 +37,7 @@ export const onClientEntry = () => {
     console.log("Search param", window.location.search)
     localStorage.setItem('search_param', window.location.search);
   }
-  
+
 }
 
 export const wrapRootElement = Boot;
