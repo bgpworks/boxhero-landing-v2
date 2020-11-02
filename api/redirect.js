bc3969020a584191547946195660a5f3e415901c
Heehong Moon(문희홍) <bbirec@gmail.com>
2020-11-02 15:29:02 +0900

    vercel config.

---
+++ api/redirect.js
@@ -0,0 +1,25 @@
+var parser = require("accept-language-parser");
+
+const urls = {
+  ko: "/ko",
+  en: "/en"
+};
+
+module.exports = (req, res) => {
+  var path = req.query["path"] || "";
+  var langs = req.headers["accept-language"];
+  var lang = parser.pick(Object.keys(urls), langs, { loose: true });
+  if (lang == null) {
+    console.log("No matching langs.");
+    lang = "en";
+  }
+  var url = urls[lang];
+
+  console.log(path);
+  console.log(langs, lang, url);
+
+  res.writeHead(302, {
+    Location: url + "/" + path
+  });
+  res.end();
+};
