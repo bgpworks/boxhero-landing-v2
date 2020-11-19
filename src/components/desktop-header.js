6905405e94fbfa819e04f8f6482c20a8c6ff65a9
Jeongbong Seo <jngbng@gmail.com>
2020-11-19 18:41:36 +0900

    편의기능/요금안내 상단 여백 버그 수정

--- src/components/desktop-header.js
+++ src/components/desktop-header.js
@@ -17,52 +17,55 @@ export const DesktopHeader = ({ isFloatMenu, curMenu }) => {
   // 여기서 이상한 워닝 뜨는건 gatsby-plugin-react-i18next의 이슈. 기능상 문제는 없는 듯. https://github.com/microapps/gatsby-plugin-react-i18next/issues/5
   const { t } = useTranslation();
   return (
-    <header
-      className={`${styles.headerContainer} ${
-        isWhite ? styles.whiteContainer : ""
-      }`}
-    >
-      <Container1024 className={styles.menuContainer}>
-        <Link to="/">
-          <img
-            src={isWhite ? svgBiBlue : svgBiWhite}
-            className={styles.biLogo}
-            alt="Home"
-          />
-        </Link>
+    <>
+      <header
+        className={`${styles.headerContainer} ${
+          isWhite ? styles.whiteContainer : ""
+        }`}
+      >
+        <Container1024 className={styles.menuContainer}>
+          <Link to="/">
+            <img
+              src={isWhite ? svgBiBlue : svgBiWhite}
+              className={styles.biLogo}
+              alt="Home"
+            />
+          </Link>
 
-        <div className={styles.padding} />
+          <div className={styles.padding} />
 
-        <Link
-          to="/about/"
-          className={curMenu === "about" ? styles.selected : ""}
-        >
-          {t("header:menuAbout")}
-        </Link>
+          <Link
+            to="/about/"
+            className={curMenu === "about" ? styles.selected : ""}
+          >
+            {t("header:menuAbout")}
+          </Link>
 
-        <Link
-          to="/features/"
-          className={curMenu === "features" ? styles.selected : ""}
-        >
-          {t("header:menuFeatures")}
-        </Link>
+          <Link
+            to="/features/"
+            className={curMenu === "features" ? styles.selected : ""}
+          >
+            {t("header:menuFeatures")}
+          </Link>
 
-        <Link
-          to="/pricing/"
-          className={curMenu === "pricing" ? styles.selected : ""}
-        >
-          {t("header:menuPricing")}
-        </Link>
+          <Link
+            to="/pricing/"
+            className={curMenu === "pricing" ? styles.selected : ""}
+          >
+            {t("header:menuPricing")}
+          </Link>
 
-        <a href={t("url:doc")}>{t("header:menuDoc")}</a>
+          <a href={t("url:doc")}>{t("header:menuDoc")}</a>
 
-        <ExternalLinkWithQuery href={urlStart}>
-          <button className={styles.loginButton}>
-            {t("header:menuLoginButton")}
-          </button>
-        </ExternalLinkWithQuery>
-      </Container1024>
-    </header>
+          <ExternalLinkWithQuery href={urlStart}>
+            <button className={styles.loginButton}>
+              {t("header:menuLoginButton")}
+            </button>
+          </ExternalLinkWithQuery>
+        </Container1024>
+      </header>
+      {!isFloatMenu && <div className={styles.headerPlaceholder}></div>}
+    </>
   );
 };
 
