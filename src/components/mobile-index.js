c58191d9452af66f3c2374295ef53d56ca395d25
Jeongbong Seo <jngbng@gmail.com>
2021-02-15 18:50:15 +0900

    gatsby-background-image 제거

--- src/components/mobile-index.js
+++ src/components/mobile-index.js
@@ -4,7 +4,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Img from "gatsby-image";
-import BackgroundImage from "gatsby-background-image";
 import { Link, Trans } from "gatsby-plugin-react-i18next";
 import ScrollContainer from "react-indiana-drag-scroll";
 import {
