51890401ae369d7b47e63d3d8988fcd435e3c4fd
BitYoungjae <bityoungjae@gmail.com>
2021-06-09 10:05:16 +0900

    package-lock.json 삭제 & download link 상수화

--- src/components/mobile-marketing-210524.js
+++ src/components/mobile-marketing-210524.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Img from "gatsby-image";
 
+const APP_DOWNLOAD_LINK =
+  "https://redirect.appmetrica.yandex.com/serve/603693493260452581";
+
 const MobileMarketing = ({ data }) => {
   return (
     <div>
       <Img fluid={data.mobile1.childImageSharp.fluid} />
-      <a href="https://redirect.appmetrica.yandex.com/serve/603693493260452581">
+      <a href={APP_DOWNLOAD_LINK}>
         <Img fluid={data.mobile2.childImageSharp.fluid} />
       </a>
       <Img fluid={data.mobile3.childImageSharp.fluid} />
