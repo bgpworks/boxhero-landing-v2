4101497441faa0e72f9133b73253e04c7d9eaf96
BitYoungjae <bityoungjae@gmail.com>
2021-06-09 10:02:38 +0900

    요청한 링크로 마케팅 페이지 내 앱 링크 변경

--- src/components/mobile-marketing-210524.js
+++ src/components/mobile-marketing-210524.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Img from "gatsby-image";
 
-import { AppDownloadLink } from "../components/common";
-
 const MobileMarketing = ({ data }) => {
   return (
     <div>
       <Img fluid={data.mobile1.childImageSharp.fluid} />
-      <AppDownloadLink>
+      <a href="https://redirect.appmetrica.yandex.com/serve/603693493260452581">
         <Img fluid={data.mobile2.childImageSharp.fluid} />
-      </AppDownloadLink>
+      </a>
       <Img fluid={data.mobile3.childImageSharp.fluid} />
 
       <a href="/">
