7ec32ab986cae8012df04bbbbfefc81f4d87bcfd
BitYoungjae <bityoungjae@gmail.com>
2021-06-09 15:09:32 +0900

    마케팅 210524 페이지에서 앱 다운로드 링크 변경 (#64)
    * 요청한 링크로 마케팅 페이지 내 앱 링크 변경
    
    * package-lock.json 삭제 & download link 상수화

--- src/components/mobile-marketing-210524.js
+++ src/components/mobile-marketing-210524.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Img from "gatsby-image";
 
-import { AppDownloadLink } from "../components/common";
+const APP_DOWNLOAD_LINK =
+  "https://redirect.appmetrica.yandex.com/serve/603693493260452581";
 
 const MobileMarketing = ({ data }) => {
   return (
     <div>
       <Img fluid={data.mobile1.childImageSharp.fluid} />
-      <AppDownloadLink>
+      <a href={APP_DOWNLOAD_LINK}>
         <Img fluid={data.mobile2.childImageSharp.fluid} />
-      </AppDownloadLink>
+      </a>
       <Img fluid={data.mobile3.childImageSharp.fluid} />
 
       <a href="/">
