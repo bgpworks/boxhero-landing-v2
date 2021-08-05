5d4ae79e0e3ba502ce5f3a3f98d7fe0a5a459747
Jeongbong Seo <jbseo@bgpworks.com>
2021-08-05 19:00:39 +0900

    스페인/인도네시아어 비콘 추가 (#73)

--- src/components/helpscout.js
+++ src/components/helpscout.js
@@ -1,6 +1,19 @@
 import { useEffect } from "react";
 import { useI18next } from "gatsby-plugin-react-i18next";
 
+function getBeaconId(language) {
+  switch (language) {
+    case "ko":
+      return "ca3bc2e0-de9e-4680-bb05-ceb9ea2535a4";
+    case "es":
+      return "4bed36b5-5fd8-4b10-95fe-a3cecb2b30f7";
+    case "id":
+      return "365effbd-c552-4e43-a2ed-63537c91e417";
+    default:
+      return "870b4827-57f0-437b-9e0b-5077cd29f127";
+  }
+}
+
 // helpscout init은 gatsby-browser에서 한번하고,
 // 번역은 helpscout beacon api의 localize 를 쓰는게 맞는 것 같은데.
 export const useHelpscout = () => {
@@ -9,10 +22,7 @@ export const useHelpscout = () => {
     // script tag는 gatsby-ssr 에서 추가함.
     // 스크립트 로드 실패를 대비한 방어 코드.
     if ("Beacon" in window) {
-      const beaconId =
-        language === "ko"
-          ? "ca3bc2e0-de9e-4680-bb05-ceb9ea2535a4"
-          : "870b4827-57f0-437b-9e0b-5077cd29f127";
+      const beaconId = getBeaconId(language);
       window.Beacon("init", beaconId);
       window.Beacon("config", {
         hideFABOnMobile: true,
