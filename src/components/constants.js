f8301a45db699bb1b8a5d7af0b99f42cde75c8ad
BitYoungjae <bityoungjae@gmail.com>
2021-08-31 15:44:32 +0900

    카카오톡 링크 변경

--- src/components/constants.js
+++ src/components/constants.js
@@ -22,6 +22,6 @@ module.exports = {
     "https://boxhero-autoupdate.s3.ap-northeast-2.amazonaws.com/BoxHero-1.0.0.dmg",
   urlFaqPaymentDomestic:
     "https://docs-ko.boxhero-app.com/docs/pricing#%EA%B5%AD%EB%82%B4-%EC%B9%B4%EB%93%9C-%EB%B0%8F-%EA%B3%84%EC%A2%8C%EC%9D%B4%EC%B2%B4-%EA%B2%B0%EC%A0%9C-%EB%B0%A9%EB%B2%95",
-  urlKakaoTalk: "https://pf.kakao.com/_rHxgpxl",
+  urlKakaoTalk: "http://pf.kakao.com/_rHxgpxl/chat",
   urlWhatsApp: "https://api.whatsapp.com/send?phone=827086704320",
 };
